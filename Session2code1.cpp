6916b2ea79a075898ff7bc53add5ab0a9d3ed502
tuanlinh31 <nguyentuanlinh311@gmail.com>
2020-10-07 23:47:43 +0700

    tuanlinh

---
+++ Session2code1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ main (){
+	int a;
+	printf("Nhap gia tri: ");
+	scanf("%d",&a);
+	if(a<=7){
+		if(a>=2){
+			printf("day la thu 2 - 7");
+		}else{
+			printf("khong phai ngay trong tuan");
+		}
+		
+	}else{
+		if(a=8){
+			printf("day la chu nhat");
+		}else{
+			printf("khong phai ngay trong tuan");
+		}
+		
+	}
+}
