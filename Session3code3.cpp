ffeb31c9787cf356825f2ff98230561f40a5b840
Linh31 <nguyentuanlinh311@gmail.com>
2020-10-08 10:31:42 +0700

    tuanlinh

---
+++ Session3code3.cpp
@@ -0,0 +1,266 @@
+#include <stdio.h>
+int main (){
+	int a;
+	printf("Nhap gia tri ngay: ");
+	scanf("%d",&a);
+	int b;
+	printf("Nhap gia tri thang: ");
+	scanf("%d",&b);
+	int c;
+	int l;
+	if(b<=12){
+		if(b>=1){
+			
+			if(b==1){
+				
+				c = a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				
+				}
+			} else if(b==2){
+				c = 31 + a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==3){
+				c = 31 + 28 + a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==4){
+				c = 31*2 +28 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==5){
+				c = 31*2 +28 +30 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==6){
+				c = 31*3 +28 +30 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==7){
+				c = 31*3 +28 +30*2 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==8){
+				c = 31*4 +28 +30*2 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==9){
+				c = 31*5 +28 +30*2 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==10){
+				c = 31*5 +28 +30*3 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} if(b==11){
+				c = 31*6 +28 +30*3 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			} else if (b==12){
+				c = 31*6 +28 +30*4 +a;
+				l = (c+7)%7;
+				if (l==1){
+					printf("thu 2");
+					
+				}if(l==2){
+					printf("thu 3");
+				}if(l==3){
+					printf("thu 4");
+					
+				}if(l==4){
+					printf("thu 5");
+				}if(l==5){
+					printf("thu 6");
+				}if(l==6){
+					printf("thu 7");
+				}else if(l==0){
+					printf("chu nhat");
+				}
+			}
+
+		
+		}else{
+			printf("Thang khong ton tai");
+		}
+		
+	}else {
+		printf("Thang khong ton tai");
+	}
+}
